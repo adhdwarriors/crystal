50962291d8b37d19c7e96f3fba302e939534dfe4
Diana Poplacenel <diana.poplacenel@berkeley.edu>
2023-04-30 00:16:12 -0700

    ??types.tsx

--- front/frontend/app/Types.tsx
+++ front/frontend/app/Types.tsx
@@ -20,7 +20,7 @@ type Type = {
     title: string;
 }
 
-const DefaultSphere: LifeSphere = {id: "0", title: "Gym"};
+const DefaultSphere: LifeSphere = {id: 0, title: "Gym"};
 const TYPES: Type[] = [{id: 0, title: "Task"}, {id: 1, title: "Goal"}, {id: 2, title: "Idea"}, {id: 3, title: "Thought"}]
 const DefaultThought: Thought = {id: 0, sphere_id: "Gym", title: "", desc: "", type_id: 3, type: "Thought"};
 const THOUGHTS: Thought[] = [{id: 0, sphere_id: "Gym", title: "Hello", desc: "I like mamba", type_id: 3, type: "Thought"}]
