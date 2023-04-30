6b6e05c6be019fee5ae9eba88c5541b4172d3f39
Justin <justin@justinf.dev>
2023-04-30 02:33:36 -0700

    Require crypto

--- backend/routes/note.route.js
+++ backend/routes/note.route.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Joi = require('joi');
 const headers = require('../scripts/headers.script');
+const crypto = require('crypto');
 
 const note_controller = require('../controllers/note.controller');
 
